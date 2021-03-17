c63b9c56858c9f5c2461845b9aa3042c1b8617c2
1106105138 <1106105138@nkust.edu.tw>
2021-03-17 22:31:14 +0800

    add program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1106105138
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
